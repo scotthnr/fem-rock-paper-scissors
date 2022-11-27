6e6f0e504e6b9d443c5db5607e5ea82268200775
scotthnr <106537126+scotthnr@users.noreply.github.com>
2022-11-27 03:53:53 +0100

    Pick a Sign + Redirect to 2nd Page

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const rules = document.getElementsByClassName("rules-display");
 
+let pickedSign = "";
+
+
+/* DISPLAY RULES */
 const displayRules = () => {
     rules[0].setAttribute("id", "display-rules");
 }
 
+/* HIDE RULES */
 const hideRules = () => {
     rules[0].setAttribute("id", "hide-rules");
+}
+
+/* PICK SIGN (ROCK, PAPER OR SCISSORS) */
+const pickSign = (className) => {
+    pickedSign = className;
+    console.log(pickedSign);
+    
+    
 }
